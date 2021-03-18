763c98be78618a369e341792591c6a376a2d848e
JustZambetti <74315721+JustZambetti@users.noreply.github.com>
2021-03-18 16:35:02 +0100

    general changes
    general changes

--- js/header.js
+++ js/header.js
@@ -5,9 +5,9 @@ window.onscroll = function () {
 function resizeHeader() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
     document.getElementById("header-nav").style.padding = "0";
-    //document.getElementById("logo").style.fontSize = "25px";
+    document.getElementById("header-menu").style.top = "64px";
   } else {
     document.getElementById("header-nav").style.padding = "10px 0";
-    //document.getElementById("logo").style.fontSize = "35px";
+    document.getElementById("header-menu").style.top = "84px";
   }
 }
