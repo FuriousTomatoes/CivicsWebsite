f496cdb51bdb2e2ea925dcffc5f38d1e85fe4032
Michelangelo1709 <michi17@outlook.it>
2021-03-19 19:51:34 +0100

    Added the theme changer

---
+++ js/theme-changer.js
@@ -0,0 +1,9 @@
+function themeChange() {
+  if (document.getElementById("csscolors").classList[0] == "dark") {
+    document.getElementById("csscolors").href = "../css/light-theme.css";
+    document.getElementById("csscolors").className = "light";
+  } else {
+    document.getElementById("csscolors").href = "../css/dark-theme.css";
+    document.getElementById("csscolors").className = "dark";
+  }
+}
