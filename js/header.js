79e5576f06f1ab7c02fcebf39847f49abe1a1747
JustZambetti <74315721+JustZambetti@users.noreply.github.com>
2021-03-20 10:55:23 +0100

    Done something

--- js/header.js
+++ js/header.js
@@ -5,7 +5,6 @@ window.onscroll = function () {
 function resizeHeader() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
     document.getElementById("header-nav").style.padding = "0";
-    document.getElementById("header-nav").ele.padding = "0";
     document.getElementById("header-menu").style.top = "64px";
   } else {
     document.getElementById("header-nav").style.padding = "10px 0";
