320579c3b834f82da2ab828d4926caedc62e87ab
JustZambetti <74315721+JustZambetti@users.noreply.github.com>
2021-03-20 16:05:37 +0100

    some changes

--- js/more.js
+++ js/more.js
@@ -1,18 +1,21 @@
+var isActive = false;
+function rotateArrow() {
+  if (isActive) {
+    document.getElementById("img-menu-arrow").style.animationPlayState =
+      "running";
+    document.getElementById("img-menu-arrow").style.animationDirection =
+      "backwards";
+  } else {
+    document.getElementById("img-menu-arrow").style.animationPlayState =
+      "running";
+    document.getElementById("img-menu-arrow").style.animationDirection =
+      "backwards";
+  }
+  isActive = !isActive;
+}
+
 function more() {
   if (document.getElementById("header-menu").style.display != "block")
     document.getElementById("header-menu").style.display = "block";
   else document.getElementById("header-menu").style.display = "none";
-  rotateArrow();
-}
-
-var isActive = false;
-function rotateArrow() {
-  if (isActive)
-    document.getElementById("img-menu-arrow").style.animation =
-      "spin1 2s infinite 2s ease";
-  else
-    document.getElementById("img-menu-arrow").style.transform =
-      "spin2 2s infinite 2s ease";
-
-  isActive = !isActive;
 }
