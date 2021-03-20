22e04ea40539fe0ca06196e6b66e352802fb5c4d
Michelangelo1709 <michi17@outlook.it>
2021-03-20 11:26:32 +0100

    fixed CSS
    Fixed CSS for themes

--- js/theme-changer.js
+++ js/theme-changer.js
@@ -1,9 +1,12 @@
+var status = dark;
 function themeChange() {
   if (document.getElementById("csscolors").classList[0] == "dark") {
     document.getElementById("csscolors").href = "../css/light-theme.css";
     document.getElementById("csscolors").className = "light";
+    status = "light";
   } else {
     document.getElementById("csscolors").href = "../css/dark-theme.css";
     document.getElementById("csscolors").className = "dark";
+    status = "dark";
   }
 }
